395c1b0074924c73754348220632c7ffa688252b
PLouisDemurger <34741719+PLouisDemurger@users.noreply.github.com>
2018-05-15 08:25:37 +0200

    Add files via upload

--- projet_web/fonction.js
+++ projet_web/fonction.js
@@ -1,6 +1,7 @@
 var largeur, hauteur, barre, connection, inscription, contenu, indice, ss_menu, banniere;
 var select1, select2, select3;
 var test;
+var test1, test2, test3, test4;
 
 
 
@@ -25,100 +26,138 @@ function init()
     ss_menu = document.getElementById("ss_menu");
     banniere = document.getElementById("bannière");
     barre.innerHTML="";
+    test1=test2=test3=test4=0;
     for(i=0;i<menu.type.length;i++)
     {
-        text="<div class='interieur' style='width:"+taille+";' id='"+menu.type[i].nom+"' >"+menu.type[i].nom+"</div>";
+        text="<div class='interieur' style='width:"+taille+";' id='"+menu.type[i].nom+"' onclick='affi_menu(this)'>"+menu.type[i].nom+"</div>";
         console.log("Texte:", text);
         barre.innerHTML+=text;
     }
     option();
 }
 
-function enl_menu()
-{
-    contenu.style.display="block";
-    indice.style.display="block";
-    ss_menu.style.display="none";
-}
-
 function affi_menu(obj)
 {
-    var i,j,k, text1, text2;
+    var j,k, text1, text2;
 
-    for (i = 0; i < menu.type.length; i++)
+    console.log(test1,test2,test3,test4);
+    if (obj.id === "Homme" && test1 === 0)
     {
-        if (obj.target.id === "ss_menu" || obj.target.className === "quart" || obj.target.className === "item" || obj.target.id === ""+menu.type[i].nom+"")
+        contenu.style.display = "none";
+        indice.style.display = "none";
+        ss_menu.style.display="block";
+        ss_menu.style.height = "" + hauteur - 89 + "";
+        ss_menu.innerHTML="";
+        text1=text2="";
+        for(j=0;j<ss_menu_homme.type.length;j++)
         {
-            contenu.style.display = "none";
-            indice.style.display = "none";
-            ss_menu.style.display="block";
-            ss_menu.style.height = "" + hauteur - 89 + "";
-        }
-        if (obj.target.id === "Homme")
-        {
-            ss_menu.innerHTML="";
-            text1=text2="";
-            for(j=0;j<ss_menu_homme.type.length;j++)
+            text1 = "<div id=" + ss_menu_homme.type[j].nom + " class='quart'><div class='titre'>" + ss_menu_homme.type[j].nom + "</div><div class='item'>";
+            for(k=0;k<ss_menu_homme.type[j].variete.length;k++)
             {
-                text1 = "<div id=" + ss_menu_homme.type[j].nom + " class='quart'><div class='titre'>" + ss_menu_homme.type[j].nom + "</div><div class='item'>";
-                for(k=0;k<ss_menu_homme.type[j].variete.length;k++)
-                {
-                    text2 ="<br>"+ss_menu_homme.type[j].variete[k].mat;
-                    text1+=text2;
-                }
-                text1+="</div></div>";
-                ss_menu.innerHTML+=text1;
+                text2 ="<br>"+ss_menu_homme.type[j].variete[k].mat;
+                text1+=text2;
             }
+            text1+="</div></div>";
+            ss_menu.innerHTML+=text1;
         }
-        if (obj.target.id === "Femme")
-        {
-            ss_menu.innerHTML="";
-            text1=text2="";
-            for(j=0;j<ss_menu_femme.type.length;j++)
-            {
-                text1 = "<div id=" + ss_menu_femme.type[j].nom + " class='quart'><div class='titre'>" + ss_menu_femme.type[j].nom + "</div><div class='item'>";
-                for(k=0;k<ss_menu_femme.type[j].variete.length;k++)
-                {
-                    text2 ="<br>"+ss_menu_femme.type[j].variete[k].mat;
-                    text1+=text2;
-                }
-                text1+="</div></div>";
-                ss_menu.innerHTML+=text1;
-            }
-        }
-        if (obj.target.id === "Enfant")
-        {
-            ss_menu.innerHTML="";
-            text1=text2="";
-            for(j=0;j<ss_menu_enfant.type.length;j++)
-            {
-                text1 = "<div id=" + ss_menu_enfant.type[j].nom + " class='quart'><div class='titre'>" + ss_menu_enfant.type[j].nom + "</div><div class='item'>";
-                for(k=0;k<ss_menu_enfant.type[j].variete.length;k++)
-                {
-                    text2 ="<br>"+ss_menu_enfant.type[j].variete[k].mat;
-                    text1+=text2;
-                }
-                text1+="</div></div>";
-                ss_menu.innerHTML+=text1;
-            }
-        }
-        if (obj.target.id === "Mixte")
-        {
-            ss_menu.innerHTML="";
-            text1=text2="";
-            for(j=0;j<ss_menu_mixte.type.length;j++)
-            {
-                text1 = "<div id=" + ss_menu_mixte.type[j].nom +" class='quart'><div class='titre'>" + ss_menu_mixte.type[j].nom + "</div><div class='item'>";
-                for(k=0;k<ss_menu_mixte.type[j].variete.length;k++)
-                {
-                    text2 ="<br>"+ss_menu_mixte.type[j].variete[k].mat;
-                    text1+=text2;
-                }
-                text1+="</div></div>";
-                ss_menu.innerHTML+=text1;
-            }
-        }
+        test2=test3=test4=0;
+        test1 = 1;
     }
+    else if (obj.id === "Homme")
+    {
+        contenu.style.display="block";
+        indice.style.display="block";
+        ss_menu.style.display="none";
+        test1 = 0;
+    }
+    if (obj.id === "Femme" && test2 === 0)
+    {
+        contenu.style.display = "none";
+        indice.style.display = "none";
+        ss_menu.style.display="block";
+        ss_menu.style.height = "" + hauteur - 89 + "";
+        ss_menu.innerHTML="";
+        text1=text2="";
+        for(j=0;j<ss_menu_femme.type.length;j++)
+        {
+            text1 = "<div id=" + ss_menu_femme.type[j].nom + " class='quart'><div class='titre'>" + ss_menu_femme.type[j].nom + "</div><div class='item'>";
+            for(k=0;k<ss_menu_femme.type[j].variete.length;k++)
+            {
+                text2 ="<br>"+ss_menu_femme.type[j].variete[k].mat;
+                text1+=text2;
+            }
+            text1+="</div></div>";
+            ss_menu.innerHTML+=text1;
+        }
+        test1=test3=test4=0;
+        test2 = 1;
+    }
+    else if (obj.id === "Femme")
+    {
+        contenu.style.display="block";
+        indice.style.display="block";
+        ss_menu.style.display="none";
+        test2 = 0;
+    }
+    if (obj.id === "Enfant" && test3 === 0)
+    {
+        contenu.style.display = "none";
+        indice.style.display = "none";
+        ss_menu.style.display="block";
+        ss_menu.style.height = "" + hauteur - 89 + "";
+        ss_menu.innerHTML="";
+        text1=text2="";
+        for(j=0;j<ss_menu_mixte.type.length;j++)
+        {
+            text1 = "<div id=" + ss_menu_enfant.type[j].nom +" class='quart'><div class='titre'>" + ss_menu_enfant.type[j].nom + "</div><div class='item'>";
+            for(k=0;k<ss_menu_enfant.type[j].variete.length;k++)
+            {
+                text2 ="<br>"+ss_menu_enfant.type[j].variete[k].mat;
+                text1+=text2;
+            }
+            text1+="</div></div>";
+            ss_menu.innerHTML+=text1;
+        }
+        test1=test2=test4=0;
+        test3 = 1;
+    }
+    else if (obj.id === "Enfant")
+    {
+        contenu.style.display="block";
+        indice.style.display="block";
+        ss_menu.style.display="none";
+        test3 = 0;
+    }
+    if (obj.id === "Mixte" && test4 === 0)
+    {
+        contenu.style.display = "none";
+        indice.style.display = "none";
+        ss_menu.style.display="block";
+        ss_menu.style.height = "" + hauteur - 89 + "";
+        ss_menu.innerHTML="";
+        text1=text2="";
+        for(j=0;j<ss_menu_mixte.type.length;j++)
+        {
+            text1 = "<div id=" + ss_menu_mixte.type[j].nom +" class='quart'><div class='titre'>" + ss_menu_mixte.type[j].nom + "</div><div class='item'>";
+            for(k=0;k<ss_menu_mixte.type[j].variete.length;k++)
+            {
+                text2 ="<br>"+ss_menu_mixte.type[j].variete[k].mat;
+                text1+=text2;
+            }
+            text1+="</div></div>";
+            ss_menu.innerHTML+=text1;
+        }
+        test1=test2=test3=0;
+        test4 = 1;
+    }
+    else if(obj.id === "Mixte")
+    {
+        contenu.style.display="block";
+        indice.style.display="block";
+        ss_menu.style.display="none";
+        test4 = 0;
+    }
+    console.log(test1,test2,test3,test4);
 }
 
 function fermeture(obj)
